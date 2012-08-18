7f76bc2a00a4808cc39e7be144c31e05ffc2802c
swallentin <stephan.wallentin@gmail.com>
2012-08-18 17:55:40 +0800

    Implement a feature that allows for navigating between items,
    Currently allows update of the view portion of the widget.
    Added styling for navigation buttons.
    Added styling for selected item and non selected items.

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -32,16 +32,39 @@
 
  Gallery.prototype = {
   to: function(position) {
+    console.log('triggered gallery.to:', position);
+    var $viewer = this.$element.find('.viewer')
+        , $children = $($viewer.children())
+        , $to = $($children[position]);
 
+    $children.removeClass('selected');
+    $to.addClass('selected');
   },
   next: function() {
-    console.log('next');
+    var targetPosition = this.selectedPosition()+1;
+
+    if( targetPosition >= this.numberOfItems() )
+      targetPosition = 0;
+
+    this.to(targetPosition);
   },
   previous: function() {
-    console.log('previous');
+    var targetPosition = this.selectedPosition()-1;
+
+    if( targetPosition <= 0 )
+      targetPosition = this.numberOfItems()-1;
+
+    this.to(targetPosition);
   },
-  add: function(item) {
-    console.log('adding');
+  selectedPosition: function() {
+    var $viewer = this.$element.find('.viewer')
+        , $selected = $viewer.find('.selected')
+        , children = $viewer.children()
+        , selectedPosition = children.index($selected);
+    return selectedPosition;
+  },
+  numberOfItems: function() {
+    return this.$element.find('.viewer').children().length;
   }
  }
 
@@ -50,16 +73,14 @@
     // this snippet will ensure that only one gallery will be instantiated
     // the instantiated gallery object will then be stored on the dom element using the $().data()
     // it will also extend the default options with the provided option
-    console.log(option);
     var $this = $(this)
     , data = $this.data('gallery')
     , options = $.extend({}, $.fn.gallery.defaults, typeof option == 'object' && option);
-    if(!data) {
-      $this.data('gallery', (data = new Gallery(this, options)));
-    if (typeof option == 'number') data.to(option)
-    else if (typeof option == 'string') data[option]()
 
-    }
+    if(!data) $this.data('gallery', (data = new Gallery(this, options)));
+    if (typeof option == 'number') data.to(option);
+    else if (typeof option == 'string') data[option]();
+
   })
  }
 
@@ -72,23 +93,12 @@
  // binding of events and stuff
  $(function() {
 
-  $('.container-thumbnail').click(function(){
-
-    $('.container-thumbnail').removeClass('selected');
-
-    $(this).addClass('selected');
-
-      // var $active = this.$element.find('.active')
-      //   , children = $active.parent().children()
-      //   , activePos = children.index($active)
-      //   , that = this
-
-
-    // // $target.gallery(option);
-
-    // e.preventDefault();
-
-    $('.current').attr('src', $(this).attr('src'));
+  $('.gallery')
+    .mouseover(function() {
+      $('.btn.thumbnail-control').stop().fadeTo(500, 1);
+    })
+    .mouseout(function() {
+      $('.btn.thumbnail-control').stop().fadeTo(500, 0.9);
   });
 
   $('.container-thumbnail')
@@ -99,25 +109,25 @@
       !$(this).hasClass('selected') && $(this).stop().fadeTo(500, 0.5);
     });
 
-  // $('.container-thumbnail').hover(function() {
-  //   $(this.)
-  // }), function() {
-
-  // });
-
-  // $('.page').click(function(){
-  //   $(this).addClass('active');
-  // });
-
   $('body').on('click.gallery.thumbnail-control', '[action]', function ( e ) {
-    // console.log('clicked');
+    // this is a bit hacky, simplied it translates into figure out the target gallery
+    // and what action to send to the target. if the action is 'to' then figure out which
+    // item to navigate to and send it as an option to the gallery
+    // default option is navigating to previous or next
     var $this = $(this)
-      , href
-      , $target = $((href = $this.attr('href')))
-      , option = $this.attr('action');
+      , target
+      , $target = $((target = $this.attr('target')))
+      , action = $this.attr('action')
+      , option = action;
+
+    if( action == 'to' ) {
+      var $selected = $(this)
+      , children = $selected.parent().children()
+      , selectedPos = children.index($selected);
+      option = selectedPos;
+    }
 
     $target.gallery(option);
-
     e.preventDefault();
   });
 
